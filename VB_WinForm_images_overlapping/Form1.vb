6784d73a1650a0cf63208a4261a7ce3aa72cf747
DerDall <niklas97d@googlemail.com>
2021-07-25 16:26:02 +0200

    added gitter

--- VB_WinForm_images_overlapping/Form1.vb
+++ VB_WinForm_images_overlapping/Form1.vb
@@ -9,8 +9,9 @@ Public Class Form1
     'Layer Anzahl = 20
     Dim layerConf(20) As Double
 
+    Dim X As String = 128
 
-
+    Dim Y As String = 128 + 40
     Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
         OpenFileDialog1.Title = "Bitte 10 Bilder auswaehlen"
         OpenFileDialog1.Multiselect = True
@@ -74,13 +75,32 @@ Public Class Form1
         FlowLayoutPanel1.DrawToBitmap(myBmp, New Rectangle(0, 0, FlowLayoutPanel1.Width, FlowLayoutPanel1.Height))
         'FlowLayoutPanel1.Dispose()
 
-        myBmp.Save("C:\_test\ausgabe.jpg")
+        myBmp.Save("C:\Users\nikla\test\image.jpg")
 
     End Sub
 
     Private Sub btnDraw_Click(sender As Object, e As EventArgs) Handles btnDraw.Click
+        OpenFileDialog2.Title = "Bitte Config file waehlen"
+        OpenFileDialog2.Multiselect = False
+        OpenFileDialog2.ShowDialog()
+        Dim p As Integer = 100
+        X = X - 200
+        Y = Y - 200
+        p = Convert.ToInt64(X)
+
+
+        Dim f As Integer = 100
+        f = Convert.ToInt64(Y)
+        MessageBox.Show(f)
+        MessageBox.Show(p)
+        ' MessageBox.Show("TEST")
         For Each pb As PictureBox In FlowLayoutPanel1.Controls
             Dim g As Graphics = pb.CreateGraphics()
+            Dim pen1 As New System.Drawing.Pen(Color.White, 1)
+            '   f = FlowLayoutPanel1.CreateGraphics
+            g.DrawLine(pen1, f, 0, f, 1280)
+            g.DrawLine(pen1, 0, p, 2000, p)
+
 
             ' Create font and brush.
             Dim drawFont As New Font("Times New Roman", 18)
@@ -92,6 +112,9 @@ Public Class Form1
 
             'get Layer Name
             Dim layerName As String
+            Dim i As String = 0
+
+
             layerName = ""
             Dim myChars() As Char = pb.Tag.ToCharArray()
             For Each ch As Char In myChars
@@ -99,9 +122,51 @@ Public Class Form1
                     layerName += ch
                 End If
             Next
+            'layer distance
+            Dim fileReader As String
+            fileReader = My.Computer.FileSystem.ReadAllText(OpenFileDialog2.FileName)
 
+            MessageBox.Show(fileReader)
+
+
+            'Dim reader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(OpenFileDialog2.FileName)
+            '  Dim Layerdistance As String
+            ' Layerdistance = ""
+
+            '  Dim line As String
+            ' Dim x As String = ""
+            ' Dim reader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(OpenFileDialog2.FileName)
+            '  Do
+            ' line = reader.ReadLine
+            'MessageBox.Show(line)
+            i = 5 * layerName + 6
+            ' MessageBox.Show(i)
+
+            '  Dim shitarray() = line.Split(";")
+            '   If i = layerName + 1 Then
+            '     MessageBox.Show(line)
+
+            '   x = String.Join(",", Arr.toarray)
+            ' For Each x As String In shitarray
+            '    shitarray.ConvertAll(x.ToArray(), New Converter(Convert.ToString))
+            '  MessageBox.Show(String.Join(shitarray))
+            '      Dim value As String = String.Join(".", shitarray)
+            '     Dim x As String = String.shitarray[2]
+            ' Next
+
+            'End If
+
+            '  MessageBox.Show(x)
+
+            '  Loop Until line Is Nothing
+
+
+
+            Dim arraylist() As String = fileReader.Split(";")
+            ' Layerdistance= arraylist (3)
             ' Draw string to screen.
-            g.DrawString("L" & layerName & " [X.Xmm]", drawFont, drawBrush, New PointF(10, 10), drawFormat)
+            ' MessageBox.Show(arraylist(1))
+            g.DrawString("L" & layerName & "  " & arraylist(i) & "mm", drawFont, drawBrush, New PointF(10, 10), drawFormat)
 
             'Todo:
             '   - draw the net
@@ -116,20 +181,41 @@ Public Class Form1
         OpenFileDialog2.Multiselect = False
         OpenFileDialog2.ShowDialog()
 
-        MessageBox.Show("TEST")
-        'Dim fileReader As String
-        'fileReader = My.Computer.FileSystem.ReadAllText(OpenFileDialog2.FileName)
+        '   MessageBox.Show("TEST")
+        Dim fileReader As String
+        fileReader = My.Computer.FileSystem.ReadAllText(OpenFileDialog2.FileName)
 
-        'MessageBox.Show(fileReader)
+        MessageBox.Show(fileReader)
 
 
-        'Dim reader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(OpenFileDialog2.FileName)
-        'Dim line As String
+        Dim reader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(OpenFileDialog2.FileName)
+        Dim line As String
+        '
+        Dim lines() = IO.File.ReadAllLines(OpenFileDialog2.FileName)
+        For Each line In lines
+            If line.Contains("Circle Mask X [px]:") Then
+                Dim positionArray() As String = line.Split(": ")
+                ' MessageBox.Show(positionArray(1))
+                X = positionArray(1)
+                MessageBox.Show(X)
+            End If
 
-        'Dim counter As Integer = 0
+            If line.Contains("Circle Mask Y [px]:") Then
+                    Dim positionArray() As String = line.Split(": ")
+                '   MessageBox.Show(positionArray(1))
+                y = positionArray(1)
+                MessageBox.Show(y)
+            End If
+
+        Next
+
+
+        '
+        ' Dim counter As Integer = 0
         'Do
-        '    line = reader.ReadLine()
-        '    Dim result As String() = Regex.Split(line, "\t")
+        'line = reader.ReadLine()
+        '  Dim result As String() = Regex.Split(line, "\t")
+        'MessageBox.Show(line)
 
         '    If result(0).GetType() = () Then
 
